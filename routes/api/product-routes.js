6c5eeefe7e9f6447e64915255ea6bc196b72759d
AhmedGarcia <166225449+AhmedGarcia@users.noreply.github.com>
2024-08-21 09:06:22 -0400

    forgot to add the node modules to the .gitignore file.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -67,7 +67,6 @@ router.post('/', (req, res) => {
 
 // update product
 router.put('/:id', (req, res) => {
-  // update product data
   Product.update(req.body, {
     where: {
       id: req.params.id,
